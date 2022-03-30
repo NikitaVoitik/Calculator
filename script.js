70cd180f9e1150d57e0a7b037022c249318795e5
TinyColonel <69681605+tinycolonel@users.noreply.github.com>
2022-03-30 13:29:02 +0300

    Finished calculator

--- script.js
+++ script.js
@@ -1,18 +1,86 @@
-let addSymbol = (number) => {
+let signs = {
+    '+': '+',
+    '-': '-',
+    '*': '*',
+    '/': '/',
+}
+
+let expression = '0';
+
+let writeToInput = () => {
     let input = document.forms.form.field;
-    if (input.value === "0")
-        input.value = "";
-    input.value+= number;
+    expression = String(expression);
+    let length = expression.length;
+    input.value = expression.substr(Math.max(0, length - 15), Math.min(length, 15));
+}
+
+let addSymbol = (number) => {
+    if (expression === '0')
+        expression = '';
+    if (number === '.' && isNaN(expression[expression.length - 1])) {
+        expression += '0';
+    }
+    expression += number;
+    writeToInput();
 }
 
 let deleteSymbol = () => {
-    let input = document.forms.form.field;
-    input.value = input.value.substr(0, input.value.length - 1);
-    if (!input.value.length)
+    expression = expression.substr(0, expression.length - 1);
+    if (!expression.length) {
         addSymbol('0');
+    }
+    else {
+        writeToInput();
+    }
 }
 
 let deleteAllSymbols = () => {
-    let input = document.forms.form.field;
-    input.value = '0';
+    expression = '0';
+    writeToInput();
+}
+
+let checkExpression = () => {
+    let kolBraces = 0;
+    for (let i = 0; i < expression.length; i++) {
+        if (expression[i] !== '(' && expression[i] !== ')')
+            continue;
+        kolBraces+= (expression[i] === '(') ? 1 : -1;
+        if (i > 0) {
+            if (expression[i] === expression[i - 1])
+                continue;
+            if (expression[i] === '('&& !(expression[i - 1] in signs)) {
+                console.log(expression[i - 1]);
+                return false;
+            }
+            if (expression[i] === ')'&& expression[i - 1] !== '.' && isNaN(expression[i - 1])) {
+                return false;
+            }
+        }
+        else if (i < expression.length - 1){
+            if (expression[i] === expression[i + 1])
+                continue;
+            if (expression[i] === '(' && isNaN(expression[i + 1])){
+                return false;
+            }
+            if (expression[i] === ')' && !(expression[i + 1] in signs)){
+                return false;
+            }
+        }
+    }
+    return !kolBraces;
+}
+
+/*
+( * + - / '(' ( 0 1 2 3 4 5 6 7 8 9
+) 0 1 2 3 4 5 6 7 8 9 .  ')' ) * + - /
+ */
+
+let calculate = () => {
+    if (!checkExpression()){
+        expression = 'error';
+        writeToInput();
+        return;
+    }
+    expression = eval(expression);
+    writeToInput();
 }
