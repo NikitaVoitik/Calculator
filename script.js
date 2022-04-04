db8d4be9a4981300fece62e0d793bfda86ed51b9
TinyColonel <69681605+NikitaVoitik@users.noreply.github.com>
2022-04-04 21:37:16 +0300

    bug fixes

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+//const signs = ['+', '-', '*', '/'];
 const signs = {
-    '+': '+',
-    '-': '-',
-    '*': '*',
-    '/': '/',
+    '+' : '+',
+    '-' : 1,
+    '/' : 2,
+    '*' : 3,
 }
+const verdicts = ['error', Infinity, NaN];
 
 let expression = '0';
 
@@ -14,21 +16,29 @@ const writeToInput = () => {
     input.value = expression.substr(Math.max(0, length - 15), Math.min(length, 15));
 }
 
-const addSymbol = (number) => {
-    if (expression === '0')
+const addSymbol = (symbol) => {
+    const length = expression.length;
+    console.log(symbol, symbol in signs);
+    if ((expression === '0' && !(symbol in signs)) || expression in verdicts)
         expression = '';
-    if (number === '.' && isNaN(expression[expression.length - 1])) {
+    if (symbol === '.' && isNaN(expression[length - 1])) {
         expression += '0';
     }
-    expression += number;
+    if (symbol in signs && expression[length - 1] in signs) {
+        expression = expression.slice(0, length - 1);
+    }
+    expression += symbol;
     writeToInput();
 }
 
 const deleteSymbol = () => {
-    expression = expression.substr(0, expression.length - 1);
+    expression = expression.slice(0, expression.length - 1);
     if (!expression.length) {
         addSymbol('0');
     } else {
+        if (expression in verdicts) {
+            expression = '0';
+        }
         writeToInput();
     }
 }
@@ -41,6 +51,11 @@ const deleteAllSymbols = () => {
 const checkExpression = () => {
     let kolBraces = 0;
     for (let i = 0; i < expression.length; i++) {
+        if (expression[i] in signs) {
+            if (!i || i === expression.length - 1 || isNaN(expression[i - 1]) || isNaN(expression[i + 1])) {
+                return false;
+            }
+        }
         if (expression[i] !== '(' && expression[i] !== ')')
             continue;
         kolBraces += (expression[i] === '(') ? 1 : -1;
@@ -54,8 +69,9 @@ const checkExpression = () => {
                 return false;
             }
         } else if (i < expression.length - 1) {
-            if (expression[i] === expression[i + 1])
+            if (expression[i] === expression[i + 1]) {
                 continue;
+            }
             if (expression[i] === '(' && isNaN(expression[i + 1])) {
                 return false;
             }
